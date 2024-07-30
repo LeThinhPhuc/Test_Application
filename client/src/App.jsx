eca4c9493ced2a8bd36bd99fff4441ebe6457c14
LeThinhPhuc <phuclethinh70@gmail.com>
2024-07-30 13:57:35 +0700

    TA-69 : Setup ReactJS + Vite project and TailwindCss

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-8xl'>Hello</div>
+    </>
+  )
+}
+
+export default App
---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
