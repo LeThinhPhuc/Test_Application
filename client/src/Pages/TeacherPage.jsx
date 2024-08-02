3bdf2747b50905b42404b986bf8cffe8735b23aa
dark-doraemon <tuandinhbi75@gmail.com>
2024-08-02 22:10:06 +0700

    LeftSideBar vs Outlet

--- client/src/Pages/TeacherPage.jsx
+++ client/src/Pages/TeacherPage.jsx
@@ -1,14 +1,14 @@
 import { Outlet } from "react-router-dom"
 import Header from "../Components/Header/Header"
-import LeftSizeBar from "../Components/LeftSideBar/LeftSizeBar"
+import LeftSideBar from "../Components/LeftSideBar/LeftSizeBar"
 
 const TeacherPage = () => {
     return (
         <>
             <Header />
             <div className="flex" >
-                <LeftSizeBar />
-                <Outlet />
+                <LeftSideBar />
+                <Outlet/>
             </div>
         </>
     )
