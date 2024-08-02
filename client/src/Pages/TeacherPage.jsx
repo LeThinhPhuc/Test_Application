2b7f116fd181c524344b00feccba7402d1daef5a
dark-doraemon <tuandinhbi75@gmail.com>
2024-08-02 22:15:12 +0700

    LeftSizeBar vs Outlet - rename LeftSizeBar to LeftSideBar

--- client/src/Pages/TeacherPage.jsx
+++ client/src/Pages/TeacherPage.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom"
 import Header from "../Components/Header/Header"
-import LeftSideBar from "../Components/LeftSideBar/LeftSizeBar"
+import LeftSideBar from "../Components/LeftSideBar/LeftSideBar"
 
 const TeacherPage = () => {
     return (
